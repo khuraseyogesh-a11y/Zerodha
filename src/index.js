ffc40112250439f14fb6dbf8c12425919011aa6b
Yogesh Khurase <khuraseyogesh@gmail.com>
2026-06-18 15:55:44 +0530

    frontend

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
+import HomePage from './landing_page/Home/HomePage';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import SignUp from './landing_page/signup/Signup';
+import AboutPage from './landing_page/about/AboutPage'
+import ProductPage from './landing_page/products/ProductPage';
+import PricingPage from './landing_page/pricing/PricingPage';
+import SupportPage from './landing_page/support/SupportPage';
+import Navbar from './landing_page/Navbar';
+import Footer from './landing_page/Footer';
+import NotFound from './landing_page/NotFound';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <div>
-      
-      <h1>hello</h1>
-    </div>
+        <BrowserRouter>
+        <Navbar />
+        <Routes>
+        <Route path='/' element={< HomePage />}></Route> 
+        <Route path='/signup' element={< SignUp />}></Route>
+        <Route path='/about' element={< AboutPage />}></Route>
+        <Route path='/product' element={< ProductPage />}></Route>
+        <Route path='/pricing' element={< PricingPage />}></Route>
+        <Route path='/support' element={< SupportPage />}></Route>
+        <Route path='*' element={< NotFound />}></Route>
 
-  </React.StrictMode>
+        </Routes>
+        <Footer />
+        </BrowserRouter>
 );
 
 
